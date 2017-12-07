e10952f31f8418ce883c803caff1166faa1ad007
Aarya Kulkarni <me@gmail.com>
2017-12-07 14:19:00 -0800

    First Day- Setting Up

---
+++ A15 Polygon Lab/IrregularPolygon.java
@@ -0,0 +1,32 @@
+import java.awt.geom.*;        //for Point2D.Double
+import java.util.ArrayList;    //for ArrayList
+import gpdraw.*;                //for DrawingTool
+/**
+ * Write a description of class IrregularPolygon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IrregularPolygon
+{
+    /*
+    private ArrayList <Point2D.Double> myPolygon;
+    
+    //constructors
+    public IrregularPolygon() {
+    }
+    
+    //public methods
+    public void add(Point2D.Double aPoint) {
+    }
+    
+    public void draw() {
+    }
+    
+    public double perimeter() {
+    }
+    
+    public double area() {
+    }
+    */
+}
